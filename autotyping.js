d07851697abf7e81c5c67bd891bf6dd91dc6fffa
Harrypathak99 <97283719+Harrypathak99@users.noreply.github.com>
2024-06-06 12:04:31 +0545

    Update autotyping.js

--- autotyping.js
+++ autotyping.js
@@ -119,7 +119,7 @@ var AutoTyping = (function (e) {
 ]).default;
 
 
-const exampleText = ['Developer', 'Designer', 'Engineer'];
+const exampleText = ['Engineer', 'Developer'];
 const exampleTyping = new AutoTyping('#text', exampleText, {
     typeSpeed: 100,
     deleteSpeed: 100,
